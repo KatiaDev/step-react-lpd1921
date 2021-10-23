97223f4f049363dd392f1c66694574d3e03d5376
KatiaDev <ecaterina.popa.2021@gmail.com>
2021-10-23 12:07:59 +0300

    Register Form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import RegisterForm from "./components/RegisterForm";
+import { Container, Heading } from "@chakra-ui/react";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container className="App">
+      <Heading m="50px" textAlign="center" color="blue.600">
+        Register Form:
+      </Heading>
+      <RegisterForm />
+    </Container>
+  );
 }
 
 export default App;
